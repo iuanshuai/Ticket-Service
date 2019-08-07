a8b341f1adbaa53004b3789115c5f7e4ac78e486
Shuai Yuan <ishuaiyuan@gmail.com>
2019-08-06 20:02:20 -0400

    Edit SequenceGenerator

--- src/main/java/me/syus/ticketservice/domain/Seat.java
+++ src/main/java/me/syus/ticketservice/domain/Seat.java
@@ -3,11 +3,14 @@ package me.syus.ticketservice.domain;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+import static javax.persistence.GenerationType.SEQUENCE;
+
 @Entity
 @Table(name = "seats")
 public class Seat {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = SEQUENCE, generator="seats_id_seq")
+    @SequenceGenerator(name="seats_id_seq", sequenceName="seats_id_seq", allocationSize = 1)
     private Long id;
 
     @Column
--- src/main/java/me/syus/ticketservice/domain/User.java
+++ src/main/java/me/syus/ticketservice/domain/User.java
@@ -2,11 +2,14 @@ package me.syus.ticketservice.domain;
 import javax.persistence.*;
 import java.util.List;
 
+import static javax.persistence.GenerationType.SEQUENCE;
+
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = SEQUENCE, generator="users_id_seq")
+    @SequenceGenerator(name="users_id_seq", sequenceName="users_id_seq", allocationSize = 1)
     private long id;
 
     @Column
